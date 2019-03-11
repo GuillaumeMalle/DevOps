2b9d9f1a788b878c8ece8f77bee861c537a85303
Guillaume Malle <guillaumemalle2@gmail.com>
2019-03-11 10:35:47 +0100

    Added routes

--- application/server.js
+++ application/server.js
@@ -21,11 +21,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.render('mapage', {});
-});
-
-//require('./routes/_')(app, jsonParser);
+require('./routes/_')(app, jsonParser);
 
 const server_http = http.createServer(app);
 server_http.listen(port, (err) => {
